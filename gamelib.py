ccbdf64fd973d31383174d96062e38f2f41400c2
Diego Essaya <dessaya@gmail.com>
2020-02-01 19:27:12 -0300

    Add link to homepage

--- gamelib.py
+++ gamelib.py
@@ -1,6 +1,8 @@
 """
 Gamelib is a pure-Python single-file library/framework for writing simple games. It is
 intended for educational purposes (e.g. to be used in basic programming courses).
+
+https://github.com/dessaya/python-gamelib
 """
 
 import tkinter as tk
